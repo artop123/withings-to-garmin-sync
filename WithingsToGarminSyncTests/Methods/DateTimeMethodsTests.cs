0ae143086dd7d1996e1d702cc8ff17e9a46b6f6d
Arto Pärnänen <66357735+artop123@users.noreply.github.com>
2024-12-19 10:33:32 +0200

    Fixes (#2)

---
+++ WithingsToGarminSyncTests/Methods/DateTimeMethodsTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace WithingsToGarminSync.Methods.Tests
+{
+	public class DateTimeMethodsTests
+	{
+		[Fact]
+		public void UnixTimeStampToDateTime_ShouldConvertUnixTimeStampToCorrectDateTime()
+		{
+			// Arrange
+			long unixTimeStamp = 1672531200; // 1. tammikuuta 2023, 00:00:00 UTC
+			var expectedDateTime = new DateTime(2023, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+			// Act
+			var result = DateTimeMethods.UnixTimeStampToDateTime(unixTimeStamp);
+			var resultAsUtc = result.ToUniversalTime();
+
+			// Assert
+			resultAsUtc.Should().Be(expectedDateTime);
+		}
+	}
+}
